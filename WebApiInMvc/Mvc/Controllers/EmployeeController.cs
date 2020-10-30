1eb002ce16cfc95bafb7b30571b328edac7f9961
parth0xz <47588043+parth0xz@users.noreply.github.com>
2020-10-30 13:40:59 +0530

    asp.net project rest api

---
+++ WebApiInMvc/Mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mvc.Models;
+using System.Web;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployeeModel> emplist;
+            HttpResponseMessage response = GlobalVariables.webapiClient.GetAsync("Employee").Result;
+            emplist = response.Content.ReadAsAsync<IEnumerable<mvcEmployeeModel>>().Result;
+
+            return View(emplist);
+        }
+        public ActionResult AddorEdit(int id = 0)
+        {
+            if (id == 0)
+         
+            return View(new mvcEmployeeModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.webapiClient.GetAsync("Employee/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcEmployeeModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddorEdit(mvcEmployeeModel emp)
+        {
+            if (emp.EmployeeId == 0)
+            { 
+            HttpResponseMessage response = GlobalVariables.webapiClient.PostAsJsonAsync("Employee", emp).Result;
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.webapiClient.PutAsJsonAsync("Employee/"+emp.EmployeeId, emp).Result;
+
+            }
+
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.webapiClient.DeleteAsync("Employee/" + id.ToString()).Result;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApiInMvc/Mvc/GlobalVariables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace Mvc
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient webapiClient = new HttpClient();
+        static GlobalVariables()
+        {
+            webapiClient.BaseAddress = new Uri("http://localhost:58115/api/");
+            webapiClient.DefaultRequestHeaders.Clear();
+            webapiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
